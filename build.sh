71c50fe179d490c5d2c074fdf488069fca492a9c
Steven Stromberg <gyozaguy@gmail.com>
2016-01-22 00:43:03 -0700

    Updated build script

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-electron-packager . Pushbullet-Electron --platform=linux --arch=x64 --version=0.36.4 --overwrite=true --asar=false --app_version=0.0.1 --appname=Pushbullet-Electron --out=releases --overwrite=true --icon=images/app.png
+APPNAME=Pushbullet-Electron
+electron-packager . $APPNAME --platform=linux --arch=x64 --version=0.36.4 --overwrite=true --asar=false --app_version=0.0.1 --appname=$APPNAME --out=releases --overwrite=true --icon=images/app.png
