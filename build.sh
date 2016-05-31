d5f6b87e03f4d8a5118cd2406c98c0e0bd8d8a03
Sidney Bofah <sidney.bofah@googlemail.com>
2016-05-31 04:42:21 +0200

    cleanup(scripts): Remove build script

--- build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-APPNAME=Pushbullet-Electron
-VERSION=0.37.3
-electron-packager . $APPNAME --platform=linux --arch=x64 --version=$VERSION --overwrite=true --asar=false --app_version=0.0.1 --appname=$APPNAME --out=releases --overwrite=true --icon=images/app.png
