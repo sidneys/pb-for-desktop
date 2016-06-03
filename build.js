a5d808319a1ef48286e79c7ad92e536f887502dc
Sidney Bofah <sidney.bofah@googlemail.com>
2016-06-03 23:05:19 +0200

    deployment(darwin): Improved platform-dependent build process, added installation-packaging for Darwin.

--- build.js
+++ build.js
@@ -1,65 +1,119 @@
 'use strict';
 
 /**
- * Commandline platform override (default: build all platforms)
- * @example > npm run build darwin
- * @example > npm run build win32
- */
-var platformOverride = process.argv.slice(3)[0];
-
-/**
  * Modules
  * External
  */
 const packager = require('electron-packager'),
-      util = require('util'),
-      path = require('path');
+    fs = require('fs'),
+    appdmg = require('appdmg'),
+    path = require('path');
+
 
 /**
  * Modules
  */
 const packageJson = require('./package.json'),
-      platform = require('./app/scripts/platform');
+    platform = require('./app/scripts/platform');
+
 
 /**
- * @constant
+ * Options for Packager
  */
-const options = {
-    'dir': path.join(__dirname),
-    'out': path.join(__dirname, packageJson.build.folder),
-    'icon': path.join(__dirname, 'icons', platform.type, 'app-icon' + platform.icon(platform.type)),
-    'platform': platformOverride || packageJson.build.platform || 'all',
-    'arch': 'all',
-    'prune': true,
-    'asar': true,
-    'overwrite': true,
-    'name': packageJson.build.name,
-    'version': packageJson.build.electron,
-    'app-version': packageJson.version,
-    'build-version': packageJson.build.number,
-    'app-bundle-id': packageJson.build.id,
-    'app-category-type': packageJson.build.category,
-    'helper-bundle-id': packageJson.build.id + '.helper',
-    'app-copyright': 'Copyright © ' + new Date().getFullYear(),
-    'version-string': {
-        'FileDescription': packageJson.description
-    }
+let getOptions = function(targetPlatform) {
+    return {
+        'dir': path.join(__dirname),
+        'out': path.join(__dirname, packageJson.build.directory),
+        'icon': path.join(__dirname, 'icons', targetPlatform, 'app-icon' + platform.icon(targetPlatform)),
+        'platform': targetPlatform,
+        'arch': 'all',
+        'prune': true,
+        'asar': true,
+        'overwrite': true,
+        'name': packageJson.build.productName,
+        'version': packageJson.build.electron,
+        'app-version': packageJson.version,
+        'build-version': packageJson.build.number,
+        'app-bundle-id': packageJson.build.id,
+        'app-category-type': packageJson.build.category,
+        'helper-bundle-id': packageJson.build.id + '.helper',
+        'app-copyright': 'Copyright © ' + new Date().getFullYear(),
+        'version-string': {
+            'FileDescription': packageJson.description
+        }
+    };
 };
+
+/**
+ * Commandline platform override (default: build all platforms)
+ * @example > npm run build darwin
+ * @example > npm run build win32
+ */
+let platformListCli = process.argv.slice(3);
+
+/**
+ * Platform Target List
+ */
+var platformList = function() {
+    if ((platformListCli !== 'undefined') && (platformListCli.length > 0)) {
+        return platformListCli;
+    }
+    return packageJson.build.platform || 'all';
+};
+
 
 /**
  * Print Info
  */
-console.log('\x1b[1mBuilding:\x1b[0m\r\n%s (%s@%s)', options.name, packageJson.name, packageJson.version);
-console.log('\x1b[1mPlatform:\x1b[0m\r\n%s', options.platform);
-console.log('\x1b[1mConfiguration:\x1b[0m\r\n%s', util.inspect(options, {showHidden: false, depth: null, colors: true}));
+console.log('\x1b[1mProject:\x1b[0m\r\n%s (%s@%s)', packageJson.build.productName, packageJson.name, packageJson.version);
+console.log('\x1b[1mTargets:\x1b[0m\r\n%s', platformList().join('\r\n'));
+
 
 /**
- * Run Build
+ * Packaging
  */
-packager(options, function(err) {
-    if (err) {
-        return console.error(err);
-    }
+var packageDarwin = function(assetLocationList, options, target) {
+    assetLocationList.forEach(function(folder) {
+        var packageFilepath = path.join(options.out, options.name + '.dmg');
 
-    console.log('\x1b[1mBuilding Complete.\x1b[0m');
+        try {
+            fs.unlinkSync(packageFilepath);
+            console.log('Previous package removed.');
+        } catch (err) {}
+
+        var packageHelper = appdmg({
+            target: packageFilepath,
+            basepath: '',
+            specification: {
+                'title': options.name,
+                'icon': options.icon,
+                'background': path.join(__dirname, 'icons', target, 'installation-background.png'),
+                'contents': [
+                    { 'x': 448, 'y': 344, 'type': 'link', 'path': '/Applications' },
+                    { 'x': 192, 'y': 344, 'type': 'file', 'path': path.join(folder, options.name + '.app') }
+                ]
+            }
+        });
+        packageHelper.on('finish', function() { console.log('\x1b[1mPackaging complete:\x1b[0m\r\n%s', packageFilepath); });
+        packageHelper.on('error', function(err) { console.error('\x1b[1mPackaging error:\x1b[0m\r\n%s', err); });
+    });
+};
+
+
+/**
+ * Building
+ */
+platformList().forEach(function(target) {
+    var options = getOptions(target);
+
+    packager(options, function(err, result) {
+        if (err) {
+            return console.error(err);
+        }
+        console.log('\x1b[1mBuilding "%s" complete. Result:\x1b[0m\r\n%s', target, result.join('\r\n'));
+
+        if (target === 'darwin') {
+            packageDarwin(result, options, target);
+        }
+    });
 });
