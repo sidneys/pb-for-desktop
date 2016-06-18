a590c7201e4aa52e5910296a4aad4f3a39ce0174
Sidney Bofah <sidney.bofah@googlemail.com>
2016-06-18 20:57:42 +0200

    module(platform.js): Streamlined platform/type/name properties.

--- app/scripts/platform.js
+++ app/scripts/platform.js
@@ -5,30 +5,31 @@
  */
 var platform = process.platform;
 
-platform = platform.indexOf('win') === 0 ? 'win'
+var type = platform.indexOf('win') === 0 ? 'win'
             : platform.indexOf('darwin') === 0 ? 'darwin'
             : 'linux';
 
-var arch = process.arch === 'ia32' ? '32' : '64';
+var arch = process.arch === 'ia32' ? '32'
+            : '64';
 
-var icon = function(type) {
+var iconExtension = function(type) {
     return type.indexOf('win') === 0 ? '.ico' : type.indexOf('darwin') === 0 ? '.icns' : '.png';
 };
 
-var image = function(type) {
+var imageExtension = function(type) {
     return type.indexOf('darwin') === 0 ? '-Template.png' : '.png';
 };
 
 
 module.exports = {
-    isDarwin: platform === 'darwin',
-    isOSX: platform === 'darwin',
-    isWind: platform === 'win',
-    isWindows: platform === 'win',
-    isLinux: platform === 'linux',
+    isDarwin: type === 'darwin',
+    isOSX: type === 'darwin',
+    isWin: type === 'win',
+    isWindows: type === 'win',
+    isLinux: type === 'linux',
     name: platform + arch,
     type: platform,
     arch: arch,
-    icon: icon,
-    image: image
+    icon: iconExtension,
+    image: imageExtension
 };
