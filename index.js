d2bab33826bee0e067b2e4844ab880ad42c385a4
Sidney Bofah <sidney.bofah@googlemail.com>
2016-06-19 21:23:29 +0200

    fix(config): Replaced configuration file module due to win32 issues.

--- index.js
+++ index.js
@@ -4,14 +4,13 @@
 const { app, BrowserWindow, Tray, Menu, shell, dialog, ipcMain } = require('electron');
 
 const path = require('path'),
-    storage = require('electron-json-storage'),
+    Configstore = require('configstore'),
     _ = require('lodash');
 
 const platform = require('./app/scripts/platform');
 
 const DEFAULT_WIDTH = 1024,
     DEFAULT_HEIGHT = 700;
-
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -29,13 +28,12 @@ let trayIconDefault = path.join(__dirname, 'icons', platform.type, 'icon-tray' +
 
 
 
-var settingsStore = storage,
-    settingsDefault = {
+var settingsDefault = {
         dock: true,
         notifyAfter: 0,
         version: app.getVersion()
     },
-    settings = {};
+    settingsStore = new Configstore('settings', settingsDefault);
 
 
 /**
@@ -92,7 +90,7 @@ var setDockVisibility = function(doShow) {
             mainWindow.hide();
         }
     }
-    settings.dock = doShow;
+    settingsStore.set('dock', doShow);
 };
 
 
@@ -100,7 +98,7 @@ var setDockVisibility = function(doShow) {
  * Dock: Toggle Visibility
  */
 var toggleDock = function() {
-    if (settings.dock !== null && settings.dock === true) {
+    if (settingsStore.get('dock') !== null && settingsStore.get('dock') === true) {
         setDockVisibility(false);
     } else {
         setDockVisibility(true);
@@ -109,65 +107,29 @@ var toggleDock = function() {
 
 
 /**
- * Settings: Store to file
- */
-var storeSettings = function(callback) {
-    settingsStore.set('settings', settings, function(error) {
-        if (error) {
-            return handleError(error);
-        }
-
-        log('[storeSettingsProperty]', 'settings', settings);
-
-        callback();
-    });
-};
-
-
-/**
- * Add new property to settings
- *
+ * Get value of global settings property.
  * @param {String} property - Property Name
- * @param {*=} value - Initial Value
  */
-var initSettingsProperty = function(property, value) {
-    var initialValue = value || null;
-    if (settings.hasOwnProperty(property)) {
+var getSetting = function(property) {
+    if (typeof property === 'undefined') {
         return;
     }
-    settings[property] = initialValue || null;
 
-    log('[initSettingsProperty]', 'property', property, 'initialValue', initialValue, 'settingsDefaults[property]', settings[property]);
+    log('[getSetting]', 'property', property, 'configDefaults[property]', settingsStore.get(property));
+
+    return settingsStore.get(property);
 };
 
 
-/**
- * Get value of global settings property. Create property if it does not exist.
- * @param {String} property - Property Name
- * @param {String} value - Value
- */
-var getSettingsProperty = function(property) {
-    if (typeof property === 'undefined') {
-        return settings;
-    }
-
-    log('[getSettingsProperty]', 'property', property, 'configDefaults[property]', settings[property]);
-    return settings[property];
-};
-
-
-var setSettingsProperty = function(property, value, callback) {
-    if (typeof property === 'undefined') {
+var setSetting = function(property, value) {
+    if (!property) {
         return false;
     }
-    settings[property] = value;
 
-    log('[setSettingsProperty]', 'property', property, 'value', value);
+    log('[setSetting]', 'property', property, 'value', value);
 
-    storeSettings(callback);
-    // return settings[property];
+    settingsStore.set(property, value || null);
 };
-
 
 
 /**
@@ -191,20 +153,21 @@ ipcMain.on('error-show', (event, message) => {
 });
 
 ipcMain.on('settings-get', (event, property) => {
-    log('[settings-get]', 'property', property);
+    var result;
 
-    if (!settings.hasOwnProperty(property)) {
-        initSettingsProperty(property);
+    if (property) {
+        result = getSetting(property);
+    } else {
+        result = settingsStore.all;
     }
-    event.sender.send('settings-get-reply', getSettingsProperty(property));
+
+    event.sender.send('settings-get-reply', result);
 });
 
 ipcMain.on('settings-set', (event, property, value) => {
     log('[settings-set]', 'property', property, 'value', value);
-
-    setSettingsProperty(property, value, function() {
-        event.sender.send('settings-set-reply', property, value);
-    });
+    setSetting(property, value);
+    event.sender.send('settings-set-reply', property, value);
 });
 
 
@@ -234,7 +197,8 @@ app.on('before-quit', () => {
 });
 
 app.on('quit', () => {
-    storeSettings();
+    log('Settings saved at:', settingsStore.path);
+    log('Settings:', settingsStore.all);
 });
 
 app.on('activate', () => {
@@ -254,95 +218,94 @@ app.on('window-all-closed', function() {
 // initialization and is ready to create browser windows.
 app.on('ready', function() {
 
-    // Load Settings from file
-    settingsStore.get('settings', function(error, result) {
-        if (error) { return handleError(error); }
+    // Commit Settings
+    log('Settings loaded from:', settingsStore.path);
+    log('Settings:', settingsStore.all);
 
-        // Commit Settings
-        settings = result;
-        _.defaults(settings, settingsDefault);
-        log('[settingsStore.get]', 'result:', result, 'settings:', settings);
+    // Init Tray
+    sysTray = new Tray(trayIconDefault);
+    sysTray.setImage(trayIconDefault);
+    sysTray.setToolTip(appName);
+    sysTray.setContextMenu(Menu.buildFromTemplate([
+        { label: 'Open', click() { mainWindow.show(); } },
+        { type: 'separator' },
+        {
+            label: 'Hide Application Window', type: 'checkbox', checked: !settingsStore.get('dock'),
+            click() { toggleDock(); }
+        },
+        { label: 'Quit', click() { app.quit(); } }
+    ]));
 
-        // Init Tray
-        sysTray = new Tray(trayIconDefault);
-        sysTray.setImage(trayIconDefault);
-        sysTray.setToolTip(appName);
-        sysTray.setContextMenu(Menu.buildFromTemplate([
-            { label: 'Open', click() { mainWindow.show(); } },
-            { type: 'separator' },
-            { label: 'Hide Application Window', type: 'checkbox', checked: !settings.dock, click() { toggleDock(); } },
-            { label: 'Quit', click() { app.quit(); } }
-        ]));
+    // Create the browser window.
+    mainWindow = new BrowserWindow(windowOptions);
 
-        // Create the browser window.
-        mainWindow = new BrowserWindow(windowOptions);
+    setDockVisibility(settingsStore.get('dock'));
 
-        setDockVisibility(settings.dock);
+    // and load the index.html of the app.
+    mainWindow.loadURL(appUrl);
 
-        // and load the index.html of the app.
-        mainWindow.loadURL(appUrl);
-
-        // Emitted when the window is closed.
-        mainWindow.on('closed', () => {
-            // Dereference the window object, usually you would store windows
-            // in an array if your app supports multi windows, this is the time
-            // when you should delete the corresponding element.
-            mainWindow = null;
-        });
-
-        mainWindow.on('focus', () => {
-            sysTray.setImage(trayIconDefault);
-        });
-
-        mainWindow.on('close', ev => {
-            if (mainWindow.forceClose) {
-                return;
-            }
-            ev.preventDefault();
-            mainWindow.hide();
-        });
-
-        mainPage = mainWindow.webContents;
-
-        mainPage.on('will-navigate', (event, url) => {
-            event.preventDefault();
-            open(url);
-        });
-
-        mainPage.on('dom-ready', () => {
-            mainWindow.show();
-            mainWindow.center();
-        });
-
-        // Create the Application's main menu
-        var template = [{
-            label: 'Application',
-            submenu: [
-                { label: 'About Application', selector: 'orderFrontStandardAboutPanel:' },
-                { type: 'separator' },
-                { label: 'Quit', accelerator: 'Command+Q', click: function() { app.quit(); } }
-            ]
-        }, {
-            label: 'Edit',
-            submenu: [
-                { label: 'Undo', accelerator: 'CmdOrCtrl+Z', selector: 'undo:' },
-                { label: 'Redo', accelerator: 'Shift+CmdOrCtrl+Z', selector: 'redo:' },
-                { type: 'separator' },
-                { label: 'Cut', accelerator: 'CmdOrCtrl+X', selector: 'cut:' },
-                { label: 'Copy', accelerator: 'CmdOrCtrl+C', selector: 'copy:' },
-                { label: 'Paste', accelerator: 'CmdOrCtrl+V', selector: 'paste:' },
-                { label: 'Select All', accelerator: 'CmdOrCtrl+A', selector: 'selectAll:' }
-            ]
-        }, {
-            label: 'Developer',
-            submenu: [
-                {
-                    label: 'Web Inspector', accelerator: 'Command+D',
-                    click: function() { mainWindow.openDevTools(); }
-                }
-            ]
-        }];
-
-        Menu.setApplicationMenu(Menu.buildFromTemplate(template));
+    // Emitted when the window is closed.
+    mainWindow.on('closed', () => {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        mainWindow = null;
     });
+
+    mainWindow.on('focus', () => {
+        sysTray.setImage(trayIconDefault);
+    });
+
+    mainWindow.on('close', ev => {
+        if (mainWindow.forceClose) {
+            return;
+        }
+        ev.preventDefault();
+        mainWindow.hide();
+    });
+
+    mainPage = mainWindow.webContents;
+
+    mainPage.on('will-navigate', (event, url) => {
+        event.preventDefault();
+        open(url);
+    });
+
+    mainPage.on('dom-ready', () => {
+        mainWindow.show();
+        mainWindow.center();
+    });
+
+    // Create the Application's main menu
+    var template = [{
+        label: 'Application',
+
+        submenu: [
+            { label: 'About Application', selector: 'orderFrontStandardAboutPanel:' },
+            { type: 'separator' },
+            { label: 'Quit', accelerator: 'Command+Q', click: function() { app.quit(); } }
+        ]
+    }, {
+        label: 'Edit',
+        submenu: [
+            { label: 'Undo', accelerator: 'CmdOrCtrl+Z', selector: 'undo:' },
+            { label: 'Redo', accelerator: 'Shift+CmdOrCtrl+Z', selector: 'redo:' },
+            { type: 'separator' },
+            { label: 'Cut', accelerator: 'CmdOrCtrl+X', selector: 'cut:' },
+            { label: 'Copy', accelerator: 'CmdOrCtrl+C', selector: 'copy:' },
+            { label: 'Paste', accelerator: 'CmdOrCtrl+V', selector: 'paste:' },
+            { label: 'Select All', accelerator: 'CmdOrCtrl+A', selector: 'selectAll:' }
+        ]
+    }, {
+        label: 'Developer',
+        submenu: [
+            {
+                label: 'Web Inspector', accelerator: 'Command+D',
+                click: function() { mainWindow.openDevTools(); }
+            }
+        ]
+    }];
+
+    Menu.setApplicationMenu(Menu.buildFromTemplate(template));
+
 });
